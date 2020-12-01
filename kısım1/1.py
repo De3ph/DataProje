a021a1c63707c6aa57a86e978163c0829641db44
De3ph <05190000108@ogrenci.ege.edu.tr>
2020-12-01 21:20:19 +0300

    1

--- kısım1/1.py
+++ kısım1/1.py
@@ -1,27 +1,40 @@
 import random
-
-def Noktali_Uzay():
+import math
     
-    Width = int(input("Width değeri : "))
-    Height = int(input("Height değeri : "))
-    Nokta_sayisi = int(input("Oluşturulacak nokta sayısı: "))
+Width = float(input("Width değeri : "))
+Height = float(input("Height değeri : "))
+Nokta_sayisi = int(input("Oluşturulacak nokta sayısı: "))
 
-    def Bos_Uzay():
-        uzay = []
+def nokta_uret(Nokta_sayisi):
 
-        for i in range(Width):
-            uzay.append([])
-            for j in range(Height):
-                uzay[i].append([])
-        return uzay
-    
-    bos_uzay = Bos_Uzay()
-
+    nx2 = []
     for i in range(Nokta_sayisi):
-        x_point = random.randint(0,Width - 1)
-        y_point = random.randint(0,Height - 1)
-        bos_uzay[x_point][y_point] = [x_point , y_point]
+        x_point = random.uniform(0,Width)
+        y_point = random.uniform(0,Height)
+        eleman = []
+        eleman.append(x_point)
+        eleman.append(y_point)
+        print(eleman)
+        nx2.append(eleman)
+    
+    return nx2
+        
+nx2 = nokta_uret(Nokta_sayisi)
 
-    return bos_uzay
+nxn = []
 
-noktali_uzay = Noktali_Uzay()
+for i in range(len(nx2)):
+    nxn.append([])
+
+    for j in range(len(nx2)):
+        dist_list = []
+        d = format(math.sqrt((nx2[i][0] - nx2[j][0])**2 + (nx2[i][1] - nx2[j][1])**2) , ".3f")
+        dist_list.append(d)
+        nxn[i].append(dist_list)
+
+i = 0
+
+while(i < len(nxn)):
+    print(nxn[i])
+    i +=1
+
